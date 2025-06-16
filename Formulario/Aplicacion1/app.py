246dabdae314a0ceca4fe40fd89cfa18164f8563
ithan <ithan.camacho@epn.edu.ec>
2025-06-16 16:56:46 -0500

    sda

---
+++ Formulario/Aplicacion1/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import config
+
+app = Flask(__name__)
+
+#clave secreta Flask para habilitar el uso de sesiones
+app.secret_key = "your_secret_key"
+
+# Configuración de la base de datos MySQL
+app.config['MYSQL_HOST'] = config.Config.MYSQL_HOST
+app.config['MYSQL_USER'] = config.Config.MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config.Config.MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config.Config.MYSQL_DB
+app.config['MYSQL_PORT'] = config.Config.MYSQL_PORT
+
+# Inicializar la base de datos
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def formulario():
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        nombre = request.form.get('name')
+        apellido = request.form.get('lastname')
+        correo = request.form.get('email')
+        mensaje = request.form.get('message')
+        pais = request.form.get('pais')
+        telefono = request.form.get('telefono')
+        edad = request.form.get('edad')
+
+        
+        # los datos, almacenarlos en una base de datos, etc.
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO datos (nombre,correo,apellido,mensaje,pais,telefono,edad) VALUES (%s, %s, %s, %s, %s, %s, %s)",(nombre, correo, apellido, mensaje, pais, telefono, edad))
+        mysql.connection.commit()
+        cur.close()
+                
+        # Redirigir al formulario
+        return redirect(url_for('formulario'))
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
